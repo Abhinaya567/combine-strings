9c2a97383a68f49b229ef5818915b63711784134
Abhinaya567 <abhinayareddy398@gmail.com>
2026-01-31 08:36:36 +0530

    Update combine.java

--- combine.java
+++ combine.java
@@ -1,20 +1,4 @@
-/*import java.util.Scanner;
-public class adding{
-public static void main (String[] args){
-Scanner sc = new Scanner(System.in);
-System.out.println("enter the first number");
-int a = sc.nextInt();
-System.out.println("enter the second number");
-int b = sc.nextInt();
-int dig1 = (a%100)/10;
-int dig2 = (b%100)/10;
-int sum = dig1+dig2;
-System.out.println("adding of last two digits of given numbers" + sum );
-}
-}*/
-
-
-/*import java.util.Scanner;
+import java.util.Scanner;
 public class combine{
 public static void main (String[] args){
 Scanner sc = new Scanner(System.in);
@@ -24,26 +8,6 @@ System.out.println("enter the second string");
 String add2 = sc.nextLine();
 System.out.println("combined string is" + add1+add2);
 }
-}*/
-
-/*import java.util.Scanner;
-public class combine{
-public static String shortLongShort(String a,String b){
-if(a.length()<b.length()){
-return a+b+a;
-}else{
-return b+a+b;
 }
-}
-public static void main (String[] args){
-Scanner sc = new Scanner(System.in);
-System.out.println("enter the first string");
-String a = sc.nextLine();
-System.out.println("enter the second string");
-String b = sc.nextLine();
-String result = shortLongShort(a,b);
-System.out.println(result);
-}
-}*/
 
 
